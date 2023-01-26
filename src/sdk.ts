62ee931e27f6e750b1089cce002153514a2f7014
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-26 12:43:59 -0800

    Minimize code src/sdk.ts

--- src/sdk.ts
+++ src/sdk.ts
@@ -1,7 +1,7 @@
 import { S3, PutObjectCommand } from "@aws-sdk/client-s3";
-import dotenv from "dotenv";
+import { config } from "dotenv";
 
-dotenv.config();
+config();
 
 const accessKeyId = process.env.ACCESS_KEY || "";
 const secretAccessKey = process.env.SECRET_ACCESS_KEY || "";
@@ -9,49 +9,22 @@ const region = process.env.REGION || "";
 const bucketName = process.env.BUCKET_NAME || "";
 
 const main = async (): Promise<void> => {
-  await performCommands();
-};
-
-const performCommands = async (): Promise<void> => {
-  await finalize(bucketName);
-};
-
-export const finalize = async (bucket: string): Promise<void> => {
-  const credentials = {
-    accessKeyId,
-    secretAccessKey,
-  };
-
-  const s3 = new S3({ credentials, region });
+  const credentials = { accessKeyId, secretAccessKey };
+  const client = new S3({ credentials, region });
 
   let counter = 0;
   while (true) {
     counter++;
     console.log("START");
-    await pushToS3(s3, bucket, Buffer.from("", "utf-8"), "test/2");
-    console.log(`DONE: ${counter}`);
-  }
-};
-
-const pushToS3 = async (
-  s3: S3,
-  bucket: string,
-  data: Buffer,
-  toFile: string
-): Promise<void> => {
-  try {
-    let contentType = "application/octet-stream";
-    await s3.send(
+    await client.send(
       new PutObjectCommand({
-        Bucket: bucket,
-        Key: toFile,
-        Body: data,
-        CacheControl: undefined,
-        ContentType: contentType ?? undefined,
+        Bucket: bucketName,
+        Key: "test",
+        Body: Buffer.from("", "utf-8"),
+        ContentType: "application/octet-stream",
       })
     );
-  } catch (e) {
-    console.log(e);
+    console.log(`DONE: ${counter}`);
   }
 };
 
